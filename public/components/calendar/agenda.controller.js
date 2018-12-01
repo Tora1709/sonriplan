efbf3c6e2d8ca8b9db43d206f6e5a16f48faff67
German <gmonteror@ucenfotec.ac.cr>
2018-12-01 14:00:34 -0600

    Visualisacion de tabla de citas

--- public/components/calendar/agenda.controller.js
+++ public/components/calendar/agenda.controller.js
@@ -18,7 +18,23 @@
     console.log(vm.agenda);
     console.log('vm.agenda');
     // vm.agenda = {};
-
+    function loadCitas(scope){
+      var citas=[];
+      var obj={};
+      
+      for (let i = 0; i < scope.agenda.citas.length; i++) {
+        obj[scope.agenda.citas[i].hora]+=','+scope.agenda.citas[i].paciente;
+      }
+      var key =Object.keys(obj)
+      for (let j = 0; j < key.length; j++) {
+        citas[j]={hora:key[j],pacientes:obj[key[j]].split(',')}
+        citas[j].pacientes.shift();
+        for (let k = citas[j].pacientes.length; k <5 ; k++) {
+          citas[j].pacientes.push(" ");
+        }
+      }
+      scope.citas=citas;
+    }
     function loadDoctores() {
       doctorService.getDoctores().then(function (response) {
         vm.doctores = response.data;
@@ -33,40 +49,44 @@
     }
     function loadAgenda() {
       vm.agenda = {
-        'day': "pato",
-        'doctor': "5bf9bd424e18cf35d8e917ff"
+        'doctor': vm.doctor._id
       };
       // vm.agenda.day="julio";
       // vm.agenda.doctor="5bf9bd424e18cf35d8e917ff";
       
-     
-      agendaService.getAgenda(vm.agenda).then(function (response) {
+      var pAgenda = {
+        doctor: vm.agenda.doctor,
+        day: vm.agenda.day,
+        citas: [
+          {
+            hora: "8:00 am-8:15 am",
+            paciente:"julio"
+          },
+          {
+            hora: "8:00 am-8:15 am",
+            paciente:"miguel"
+          },
+          {
+            hora: "8:15 am-8:30 am",
+            paciente:"Armando"
+          },
+          {
+            hora: "8:30 am-8:45 am",
+            paciente:"rodrigo"
+          }
+        ]
+      }
+      agendaService.getAgenda(pAgenda).then(function (response) {
         console.log(response);
         if (response.data.length==0) {
-          var pAgenda = {
-            doctor: vm.agenda.doctor,
-            day: vm.agenda.day,
-            citas: [
-              {
-                hora: "8:00 am-8:15 am",
-                paciente:""
-              },
-              {
-                hora: "8:15 am-8:30 am",
-                paciente:""
-              },
-              {
-                hora: "8:30 am-8:45 am",
-                paciente:""
-              }
-            ]
-          }
+         
          // console(pAgenda.citas[1]);
           agendaService.setAgenda(pAgenda);
         } else {
           vm.agenda = response.data[0];
+          loadCitas(vm);
         }
-      })
+      });
     }
     function clean() {
       vm.agenda = {}
